aa0cc57ea12638f63b7593d4d419d9248cf6e0e0
Adam Marcus <adammarcus930@gmail.com>
2020-05-16 22:07:46 -0700

    First Commit

---
+++ DSHelper/CatEncoder.py
@@ -0,0 +1,43 @@
+import numpy as np
+import math
+class CatEncoder:
+    def __init__(self):
+         print("CatEncoder")
+
+    def OrdinalCalc(df,col,y):
+        # For each categorical value create a numeric relationship to the dependent variable
+        distributions = df.groupby(col).sum()[y]/df.groupby([col]).count()[y]
+
+        # Because these distributions can be the same for categorical values ensure they are unique
+        # This will maintain that these values are distinct categories and not continous values
+
+        # Determine how many digits the categories could potentially go to
+        max_digits = 10 ** math.floor(np.log10(len(distributions)))
+        # Add Identifier to the end of the distribution
+        ids = [(i+1)/(10000 * max_digits) for i in range(len(distributions))]
+        replacements = round(distributions, 3) + ids
+        replacements = round(replacements, 8) #eliminates rounding issues from the ids
+        replacements = replacements.to_dict()
+        return replacements
+
+
+def GetOrdinal(df_train,y,df_test = None,cols=df_train.columns):
+
+    #Remove the independent variable if it is there
+    if y in cols:
+        cols = list(cols)
+        cols.remove(y)
+
+    categories = dict()
+    for col in cols:
+        if df_train[col].dtypes == object:
+            replacements = OrdinalCalc(df_train,col,y)
+            df_train[col] = df_train[col].map(replacements)
+            categories[col] = replacements
+
+    if df_test is not None:
+        for col in categories:
+            df_test[col] = df_test[col].map(categories[col])
+            #Replace Nulls
+
+    return categories
